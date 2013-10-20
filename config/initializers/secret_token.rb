fdf2c10bfe0295a287f750050d6f6780a47c1897
sergivillanueva <sergi.villanueva@gmail.com>
2013-10-20 12:42:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FootballTube::Application.config.secret_key_base = '9a74db67bab79dee0c3eb6b658b8f047fbec14c70f7ec36a71c09143be0d18a79d39505651b5189dc330e1eac549b10adfcdd57e357b72df9f2285778d79089e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FootballTube::Application.config.session_store :cookie_store, key: '_football_tube_session'
