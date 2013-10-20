b5d4b6ba17e4ff10762e994bf29deee5d125bda0
sergivillanueva <sergi.villanueva@gmail.com>
2013-10-20 22:46:59 +0200

    Add resizing on emblem uploader using mini magick

--- app/uploaders/emblem_uploader.rb
+++ app/uploaders/emblem_uploader.rb
@@ -1,7 +1,15 @@
 # encoding: utf-8
 
 class EmblemUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
+  process :resize_to_fit => [200, 200]
+
+  version :thumb do
+    process :resize_to_fill => [80,80]
+  end
+  
+  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
