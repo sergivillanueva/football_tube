6cba854424e805dea409bcd3b2f1d0814f84a289
sergivillanueva <sergi.villanueva@gmail.com>
2013-11-02 13:13:57 +0100

    Adding controller and views for Match

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,24 @@
+class MatchesController < ApplicationController
+  def new
+    @match = Match.new
+  end
+    
+  def create
+    @match = Match.new match_params
+    if @match.save
+      redirect_to matches_path
+    else
+      render :new
+    end
+  end
+  
+  def index
+    @matches = Match.all
+  end
+  
+  private
+  
+  def match_params
+    params.require(:match).permit(:home_team_id, :away_team_id, :home_score, :away_score, :playing_date, :playing_date)
+  end
+end
