2182670aff8fe3f7a243c198000525ba387b58ef
sergivillanueva <sergi.villanueva@gmail.com>
2013-11-02 13:13:23 +0100

    Fixing match association with teams

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,4 @@
 class Match < ActiveRecord::Base
-  has_one :home_team, class_name: :club, foreign_key: :home_team_id
-  has_one :away_team, class_name: :club, foreign_key: :away_team_id
-  
-  attr_accessible :home_score, :away_score, :playing_date
+  belongs_to :home_team, class_name: Team
+  belongs_to :away_team, class_name: Team
 end
