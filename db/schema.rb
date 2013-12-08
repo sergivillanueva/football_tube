4357fbe8eeac0538be8030307b28fe7bf54ef85a
sergivillanueva <sergi.villanueva@gmail.com>
2013-12-08 12:18:28 +0100

    Adding full_name attribute to Player

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206120526) do
+ActiveRecord::Schema.define(version: 20131208105425) do
 
   create_table "countries", force: true do |t|
     t.string  "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20131206120526) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_name"
   end
 
   create_table "teams", force: true do |t|
