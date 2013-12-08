0542eaec7bea5db51f5ce86c7185b10294ccbbb0
sergivillanueva <sergi.villanueva@gmail.com>
2013-12-08 13:51:58 +0100

    Improve player autocomplete elements showing flag

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,12 +2,13 @@ class Player < ActiveRecord::Base
   has_many :player_participations
   has_many :matches, through: :player_participations
   belongs_to :country
-
   
   def as_json(options={})
     { 
       value: self.name,
-      label: "#{self.full_name}, #{self.name}"
+      full_name: self.full_name,
+      name: self.name,
+      country_code: self.country.try(:code)
     }
   end
 end
