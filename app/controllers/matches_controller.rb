c022d2c95cf425f8cd42f583b44576fb3bc830bc
sergivillanueva <sergi.villanueva@gmail.com>
2013-12-15 13:53:04 +0100

    Fixing player participations assignment on match create

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -16,17 +16,24 @@ class MatchesController < ApplicationController
   def create
     home_team = Team.find_or_create_by({ name: params[:match][:home_team_name] })
     away_team = Team.find_or_create_by({ name: params[:match][:away_team_name] })
-    params[:match][:home_player_participations_attributes].each do |player_participation|
-      player = Player.find_or_create_by({ name: player_participation[1][:player_name] })
-      player_participation[1].merge!({ player_id: player.id }) #TODO use player
-      match_params.merge!(Hash.new(key: player_participation[0], value: player_participation[1]))
+    
+    %w(home_starters_attributes home_reserves_attributes away_starters_attributes away_reserves_attributes).each do |attributes|
+      params[:match][attributes].each do |player_participation|
+        player = Player.find_or_create_by({ name: player_participation[1][:player_name] })
+        player_participation[1].merge!({ player_id: player.id })
+        match_params.merge!(Hash.new(key: player_participation[0], value: player_participation[1]))
+      end
     end
-    params[:match][:away_player_participations_attributes].each do |player_participation|
-      player = Player.find_or_create_by({ name: player_participation[1][:player_name] })
-      player_participation[1].merge!({ player_id: player.id }) #TODO use player
-      match_params.merge!(Hash.new(key: player_participation[0], value: player_participation[1]))
+    
+    %w(home_coach_attributes away_coach_attributes).each do |attributes|
+      player_participation = params[:match][attributes]
+      player = Player.find_or_create_by({ name: player_participation[:player_name] })
+      player_participation.merge!({ player_id: player.id })
+      match_params.merge!(Hash.new(key: attributes, value: player_participation))
     end
+    
     @match = Match.new match_params.merge!({ home_team: home_team, away_team: away_team })
+    
     if @match.save
       redirect_to matches_path
     else
@@ -45,6 +52,18 @@ class MatchesController < ApplicationController
   private
   
   def match_params
-    params.require(:match).permit(:home_score, :away_score, :playing_date, :home_team_name, :away_team_name, home_player_participations_attributes: [:player_name, :side, :player_id, :team_number], away_player_participations_attributes: [:player_name, :side, :player_id])
+    params.require(:match).permit(
+      :home_score, 
+      :away_score, 
+      :playing_date, 
+      :home_team_name, 
+      :away_team_name, 
+      home_starters_attributes: [:player_name, :side, :player_id, :team_number, :role],
+      away_starters_attributes: [:player_name, :side, :player_id, :team_number, :role],
+      home_reserves_attributes: [:player_name, :side, :player_id, :team_number, :role],
+      away_reserves_attributes: [:player_name, :side, :player_id, :team_number, :role],
+      home_coach_attributes: [:player_name, :side, :player_id, :role],
+      away_coach_attributes: [:player_name, :side, :player_id, :role]
+    )
   end
 end
