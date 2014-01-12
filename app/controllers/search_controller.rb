395a93a97b62611732babf6b52d1068a988cab88
sergivillanueva <sergi.villanueva@gmail.com>
2014-01-12 12:10:19 +0100

    Show searched player name on search results view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def search_by_player
+    @player_name = params[:player_name]
     @player_participations = PlayerParticipation.where(player_id: params[:player_id]).decorate
     render :search
   end
