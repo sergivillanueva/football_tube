76d52d14e421f4a47db32d5f1382db4810277e0a
sergivillanueva <sergi.villanueva@gmail.com>
2014-02-16 22:05:58 +0100

    Adding competition_type table and reference on competitions

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119192839) do
+ActiveRecord::Schema.define(version: 20140216164855) do
+
+  create_table "competition_types", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "competitions", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "competition_type_id"
+    t.integer  "country_id"
   end
 
   create_table "countries", force: true do |t|
