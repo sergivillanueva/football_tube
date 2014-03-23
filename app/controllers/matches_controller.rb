93a03ed16881b3d5e4783fd6864193d95d637be2
sergivillanueva <sergi.villanueva@gmail.com>
2014-03-23 12:12:28 +0100

    Adding match updating

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,16 +3,7 @@ class MatchesController < ApplicationController
 
   def new
     @match = Match.new
-    11.times do
-      @match.home_starters.build({ side: "home", role: "starter" }) #TODO check if params are needed
-      @match.away_starters.build({ side: "away", role: "starter" })
-    end
-    6.times do
-      @match.home_reserves.build({ side: "home", role: "reserve" })
-      @match.away_reserves.build({ side: "away", role: "reserve" })
-    end
-    @match.build_home_coach
-    @match.build_away_coach
+    build_player_participations
   end
     
   def create
@@ -60,6 +51,28 @@ class MatchesController < ApplicationController
     end
   end
   
+  def update
+    @match = Match.find(params[:id])
+  
+    # Destroy current player_participations since they will be created again to update data
+    @match.player_participations.destroy_all
+
+    assign_players_to_match
+    assign_coaches_to_match
+        
+    @match.update_attributes match_params.merge({ 
+      home_team_id: Team.find_or_create_by({ name: params[:match][:home_team_name] }).id, 
+      away_team_id: Team.find_or_create_by({ name: params[:match][:away_team_name] }).id,
+      competition_id: Competition.find_or_create_by({ name: params[:match][:competition_name] }).id
+    })
+    
+    if @match.valid?
+      redirect_to matches_path
+    else
+      render :edit
+    end
+  end
+
   def index
     @matches = Match.order(:playing_date).decorate
   end
@@ -67,6 +80,11 @@ class MatchesController < ApplicationController
   def show
     @match = Match.find(params[:id])
     @related_matches = @match.related_matches
+  end
+
+  def edit
+    @match = Match.find(params[:id])
+    build_player_participations
   end
 
   def destroy
@@ -107,5 +125,50 @@ class MatchesController < ApplicationController
   def coaches_attributes
     [ { key: "home_coach_attributes", side: "home", role: "coach" },
       { key: "away_coach_attributes", side: "away", role: "coach" } ]
+  end
+
+  def build_player_participations
+    (11 - @match.home_starters.count).times do
+      @match.home_starters.build({ side: "home", role: "starter" }) #TODO check if params are needed
+    end
+    (11 - @match.away_starters.count).times do
+      @match.away_starters.build({ side: "away", role: "starter" })
+    end
+    (6 - @match.home_reserves.count).times do
+      @match.home_reserves.build({ side: "home", role: "reserve" })
+    end
+    (6 - @match.away_reserves.count).times do
+      @match.away_reserves.build({ side: "away", role: "reserve" })
+    end
+    @match.build_home_coach unless @match.home_coach.present?
+    @match.build_away_coach unless @match.away_coach.present?
+  end
+
+  def assign_players_to_match
+    # Assign player and secure side and role attributes for players
+    players_attributes.each do |attributes|
+      params[:match][attributes[:key]].each do |player_participation|
+        #Find player or create a new one
+        player_id = player_participation[1][:player_id]
+        player_id = Player.create({ name: player_participation[1][:player_name] }).id unless player_id.present?
+        
+        player_participation[1].merge!({ player_id: player_id, side: attributes[:side], role: attributes[:role] })
+        match_params.merge!(Hash.new(key: player_participation[0], value: player_participation[1]))
+      end
+    end
+  end
+
+  def assign_coaches_to_match
+    # Assign player and secure side and role attributes for coaches
+    coaches_attributes.each do |attributes|
+      player_participation = params[:match][attributes[:key]]
+
+      #Find player or create a new one      
+      player_id = player_participation[:player_id]
+      player_id = Player.create({ name: player_participation[:player_name] }).id unless player_id.present?
+      
+      player_participation.merge!({ player_id: player_id, side: attributes[:side], role: attributes[:role] })
+      match_params.merge!(Hash.new(key: attributes[:key], value: player_participation))
+    end
   end
 end
