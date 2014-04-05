8727802477fab573ea25aef1e038a6245554c559
sergivillanueva <sergi.villanueva@gmail.com>
2014-04-05 14:03:58 +0200

    Adding own_goal text on match show view goals section

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,16 @@
 class Goal < ActiveRecord::Base
   belongs_to :player
   belongs_to :match
+
+  def own_goal?
+  	self.own_goal == true
+  end
+
+  def side
+  	if self.match.home_players.map(&:player).include?(self.player)
+  	  self.own_goal? ? "away" : "home"
+  	else
+  	  self.own_goal? ? "home" : "away"
+  	end
+  end
 end
