50c2dc86065095721eb917731c591ae0fbe19d1d
sergivillanueva <sergi.villanueva@gmail.com>
2014-04-13 21:54:03 +0200

    Paginate players index view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,7 @@ class PlayersController < ApplicationController
   end
   
   def index
-    @players = Player.order("name")
+    @players = Player.order("name").paginate(:page => params[:page], :per_page => 500)
   end
   
   def edit
