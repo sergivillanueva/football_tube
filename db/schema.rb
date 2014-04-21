3d4413847b2cd0a9329cfdb8b01f570dde29152d
Ubuntu <ubuntu@ip-172-31-4-220.us-west-2.compute.internal>
2014-04-21 14:24:50 +0000

    Solved conflict on Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -97,8 +97,8 @@ ActiveRecord::Schema.define(version: 20140421101828) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "videos", force: true do |t|
     t.string   "source_file"
