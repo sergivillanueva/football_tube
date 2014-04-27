f4939145db855bec1554fefb2f514df8489829e1
sergivillanueva <sergi.villanueva@gmail.com>
2014-04-27 11:07:47 +0200

    Order matches by playing_date on search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def search_by_player
     @player_name = params[:player_name]
     if params[:player_id].present?
-      @player_participations = PlayerParticipation.where(player_id: params[:player_id]).decorate  
+      @player_participations = PlayerParticipation.joins(:match).where(player_id: params[:player_id]).order("matches.playing_date").decorate  
     else
       @players = Player.where("name like '%#{params[:player_name]}%' OR full_name like '%#{params[:player_name]}%'").decorate
       render :player_results
@@ -15,7 +15,7 @@ class SearchController < ApplicationController
   def search_by_team
     @team_name = params[:team_name]
     if params[:team_id].present?
-      @matches = Match.where("home_team_id = #{params[:team_id]} OR away_team_id = #{params[:team_id]}").decorate  
+      @matches = Match.where("home_team_id = #{params[:team_id]} OR away_team_id = #{params[:team_id]}").order("playing_date").decorate  
     else
       @teams = Team.where("name like '%#{params[:team_name]}%' OR nick_names like '%#{params[:team_name]}%'").decorate
       render :team_results
