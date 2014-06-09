a3759aaff2e08bad1faf18cbadc75822d7f2dd70
sergivillanueva <sergi.villanueva@gmail.com>
2014-06-09 17:22:32 +0200

    Adding scope and zone to competitions form
    Also adding more options to competitions kind options_for helper and
    grouping it for show the options on a more friendly way.

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -1,8 +1,29 @@
 module CompetitionsHelper
-  def options_for_competition_kind
-  	["league_1", "league_2", "local_cup", "champions_league", "cup_winners_cup", "uefa_cup", "world_cup", "euro", 
-  	"african_cup", "international_supercup", "friendly"].map do |l|
-  	  [I18n.t("competitions.kind.#{l}"), l]
+  def national_teams_competitions_kinds
+  	%w(world_cup world_cup_u20 euro euro_u21 copa_america african_cup international_supercup confederations_cup olympic_games mundialito)
+  end
+  def international_competitions_kinds
+  	%w(champions_league cup_winners_cup uefa_cup international_cup libertadores conmebol mercosur)
+  end
+  def domestic_competitions_kinds
+  	%w(league_1 league_2 cup supercup)
+  end
+  def grouped_options_for_competition_kind
+  	{
+  	  t("competitions.scope.national_teams") => national_teams_competitions_kinds.map{|l| [I18n.t("competitions.kind.#{l}"), l]},
+  	  t("competitions.scope.international") => international_competitions_kinds.map{|l| [I18n.t("competitions.kind.#{l}"), l]}, 
+  	  t("competitions.scope.domestic") => international_competitions_kinds.map{|l| [I18n.t("competitions.kind.#{l}"), l]}, 
+  	  'friendly' => [[I18n.t("competitions.kind.friendly"), 'friendly']]
+  	}
+  end
+  def options_for_competition_scope
+  	%w(national_teams international domestic).each do |s|
+  	  [t("competitions.scope.#{s}"), s]
   	end
+  end
+  def options_for_competition_zone
+  	%w(world europe south_america).each do |z|
+  	  [t("competitions.zone.#{z}"), z]
+  	end  	
   end
 end
