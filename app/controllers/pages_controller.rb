b0ab3e90b820922a6e9d87111aa20b85b276cf59
sergivillanueva <sergi.villanueva@gmail.com>
2014-06-09 17:24:37 +0200

    Adding browse competitions section at home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   
   def home
-    
+    competitions = Competition.completed.group_by(&:scope)
+
+    @national_teams_competitions = competitions["national_teams"].group_by(&:zone) if competitions["national_teams"].present?
+    @international_competitions = competitions["international"].group_by(&:zone) if competitions["international"].present?
+    @domestic_competitions = competitions["domestic"].group_by(&:zone) if competitions["domestic"].present?
   end
       
 end
