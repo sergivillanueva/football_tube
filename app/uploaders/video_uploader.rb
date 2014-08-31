87fcad3063aaf546e0f5eab10650eae94d96f1f1
sergivillanueva <sergi.villanueva@gmail.com>
2014-08-31 18:18:09 +0200

    Using libfdk_aac audio encoder

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -10,5 +10,5 @@ class VideoUploader < CarrierWave::Uploader::Base
   # storage :file
   storage :fog
 
-  process encode_video: [:mp4, resolution: :same,  custom: '-vcodec libx264 -preset ultrafast' ]
+  process encode_video: [:mp4, resolution: :same,  custom: '-vcodec libx264 -acodec libfdk_aac -preset ultrafast' ]
 end
