79d94c9d9b06e9b7794639019f9825ff32e6f2da
sergivillanueva <sergi.villanueva@gmail.com>
2014-08-31 20:29:29 +0200

    Setting S3 bucket region on fog config

--- config/initializers/carrierwave_fog.rb
+++ config/initializers/carrierwave_fog.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['S3_KEY'],                        # required
     :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
+    :region => 'us-west-2'
   }
   config.fog_directory  = 'footballia'                     # required
   config.fog_public     = true
