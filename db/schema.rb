f2b9c2e919280665e1f32db71f30b511e5f1b5da
sergivillanueva <sergi.villanueva@gmail.com>
2014-11-30 21:22:50 +0100

    Adding role to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609121457) do
+ActiveRecord::Schema.define(version: 20141130201644) do
 
   create_table "competitions", force: true do |t|
     t.string   "name"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20140609121457) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
