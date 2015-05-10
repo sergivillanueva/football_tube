faf85f537c57aa24289a7b1a1256d6b1a7f57571
sergivillanueva <sergi.villanueva@gmail.com>
2015-05-10 19:16:40 +0200

    Apply Helena template on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   
   def home
     @last_match_with_video = Video.last.match
-    @best_matches = Match.joins("LEFT JOIN rating_caches ON (matches.id = rating_caches.cacheable_id) AND (rating_caches.cacheable_type = 'Match')").order("rating_caches.avg DESC, #{Rails.env.production? ? 'RAND()' : 'RANDOM()'}").limit(4).decorate
+    @best_matches = Match.joins("LEFT JOIN rating_caches ON (matches.id = rating_caches.cacheable_id) AND (rating_caches.cacheable_type = 'Match')").order("rating_caches.avg DESC, #{Rails.env.production? ? 'RAND()' : 'RANDOM()'}").limit(8).decorate
 
     @matches_count = Match.count
     @matches_with_videos_count = Match.with_videos.count.length
