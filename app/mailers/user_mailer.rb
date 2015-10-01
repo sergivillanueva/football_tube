d5be2da4a41ae3ebc158c7eb4f473fe15c87b8ec
Sergi Villanueva <sergi.villanueva@gmail.com>
2015-10-01 08:45:13 +0000

    Adding admin email account on new comment notification email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
 
   def new_comment_email comment
     @comment = comment
-    mail(to: "sergi.villanueva@gmail.com", subject: "Footballia - New comment", from: "Footballia.net <info@footballia.net>") do |format|
+    mail(to: "sergi.villanueva@gmail.com;wislabe@gmail.com", subject: "Footballia - New comment", from: "Footballia.net <info@footballia.net>") do |format|
       format.html { render layout: 'email' }
     end
   end
