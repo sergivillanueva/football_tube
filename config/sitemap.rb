87cfd061b38f3b4df58c574109aa2539b9027745
Sergi Villanueva <sergi.villanueva@gmail.com>
2016-03-07 15:56:35 +0100

    Adding localized routes on sitemap generator

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -25,39 +25,43 @@ SitemapGenerator::Sitemap.create do
   #     add article_path(article), :lastmod => article.updated_at
   #   end
 
-  add root_path, :changefreq => 'daily', :priority => 0.9
+  I18n.available_locales.each do |locale|
+    I18n.with_locale(locale) do
 
-  Match.published.find_each do |match|
-    add match_path(match), :lastmod => match.updated_at
+      add root_path, :changefreq => 'daily', :priority => 0.9
+
+      Match.published.find_each do |match|
+        add match_path(match), :lastmod => match.updated_at
+      end
+
+      Team.find_each do |team|
+        add team_path(team), :lastmod => team.updated_at
+      end
+
+      Competition.find_each do |competition|
+        add competition_path(competition), :lastmod => competition.updated_at
+      end
+
+      Player.find_each do |player|
+        add player_path(player), :lastmod => player.updated_at
+      end
+
+      add advanced_search_path
+      add search_for_clockwork_orange_path
+      add search_for_brazil_70_path
+      add search_for_dream_team_path
+      add search_for_quinta_del_buitre_path
+      add search_for_sacchi_milan_path
+      add search_for_france_80s_path
+      add search_for_spain_08_12_path
+      add search_for_el_clasico_path
+      add search_for_world_cup_finals_path
+      add search_for_champions_league_finals_path
+      add search_for_european_cup_finals_path
+      add search_libertadores_finals_path
+
+      add about_path
+      add contact_path
+    end
   end
-
-  Team.find_each do |team|
-    add team_path(team), :lastmod => team.updated_at
-  end
-
-  Competition.find_each do |competition|
-    add competition_path(competition), :lastmod => competition.updated_at
-  end
-
-  Player.find_each do |player|
-    add player_path(player), :lastmod => player.updated_at
-  end
-
-  add advanced_search_path
-  add search_for_clockwork_orange_path
-  add search_for_brazil_70_path
-  add search_for_dream_team_path
-  add search_for_quinta_del_buitre_path
-  add search_for_sacchi_milan_path
-  add search_for_france_80s_path
-  add search_for_spain_08_12_path
-  add search_for_el_clasico_path
-  add search_for_world_cup_finals_path
-  add search_for_champions_league_finals_path
-  add search_for_european_cup_finals_path
-  add search_libertadores_finals_path
-
-  add about_path
-  add contact_path
-
 end
