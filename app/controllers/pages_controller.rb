bdf6de0aff127972cffebfb8e3794305ea3abc43
Sergi Villanueva <sergi.villanueva@gmail.com>
2016-12-08 19:44:58 +0100

    Remove shit code to make search working on development with sqlite

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     #@last_matches_with_video = Video.joins(:match).where(:"matches.published" => true).order("created_at DESC").limit(7).reverse
     @last_matches_with_video = Video.available.joins(:match).select("DISTINCT videos.match_id, matches.*").where(:"matches.published" => true).order("videos.created_at DESC").limit(7).reverse.map(&:match)
     @last_match_with_video = @last_matches_with_video.pop
-    @best_matches = Match.joins("LEFT JOIN rating_caches ON (matches.id = rating_caches.cacheable_id) AND (rating_caches.cacheable_type = 'Match')").order("rating_caches.avg DESC, #{Rails.env.production? ? 'RAND()' : 'RANDOM()'}").limit(15).decorate
+    @best_matches = Match.joins("LEFT JOIN rating_caches ON (matches.id = rating_caches.cacheable_id) AND (rating_caches.cacheable_type = 'Match')").order("rating_caches.avg DESC, RAND()").limit(15).decorate
 
     @matches_count = Match.count
     @matches_with_videos_count = Match.with_videos.count.length
