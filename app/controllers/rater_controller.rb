88ab369dfb9a264bb1f91a3ad52592d652114738
Sergi Villanueva <sergi.villanueva@gmail.com>
2016-12-30 10:13:04 +0100

    Adding rates to admin

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -1,4 +1,5 @@
 class RaterController < ApplicationController
+  before_action :check_admin_role, only: [:index]
 
   def create
     if user_signed_in?
@@ -8,6 +9,18 @@ class RaterController < ApplicationController
       render :json => true
     else
       render :json => false
+    end
+  end
+
+  def index
+    respond_to do |format|
+      format.html do
+        render "rates/index"
+      end
+      format.json do
+        @rates = Rate.includes(:rater).includes(:rateable)
+        render "rates/index"
+      end
     end
   end
 end
