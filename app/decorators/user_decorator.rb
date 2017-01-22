3724a64ff6a8564e8e1c5c752ca6d976070073ca
Sergi Villanueva <sergi.villanueva@gmail.com>
2017-01-22 11:20:01 +0100

    Provide a default avatar and fixing issues on comments list

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,11 @@
 class UserDecorator < ApplicationDecorator
   def name
-    object.nick_name || "Footballia user"
+    return object.nick_name if object.nick_name.present?
+    "Footballia user"
   end
 
   def unique_name
-    object.nick_name || "Footballia user ##{object.created_at.to_i}"
+    return object.nick_name if object.nick_name.present?
+    "Footballia user ##{object.created_at.to_i}"
   end
 end
