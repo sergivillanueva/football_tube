c58618b335d0ff9fc80c182051c13e44f2152150
Sergi Villanueva <sergi.villanueva@gmail.com>
2017-05-27 10:15:37 +0200

    Prevent from destroying videos with goals attached

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,12 @@
 class Video < ActiveRecord::Base
+  has_many :goals
   belongs_to :match
   mount_uploader :source_file, VideoUploader
 
   scope :available, -> { joins(:match).where("matches.playing_date < '#{30.days.ago}'") }
+
+  def destroy
+    raise "Cannot delete video with goals" unless self.goals.count == 0
+    super
+  end
 end
